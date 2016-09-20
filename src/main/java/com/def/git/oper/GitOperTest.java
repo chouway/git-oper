c2ff16863db0b0b7ebca247c06a8e12893b1e63b
zhouyw <279704064@qq.com>
2016-09-20 21:50:14 +0800

    add test junit

---
+++ src/main/java/com/def/git/oper/GitOperTest.java
@@ -0,0 +1,17 @@
+package com.def.git.oper;
+
+import org.junit.Test;
+
+/**
+ * GitOperTest
+ * @author zhouyw
+ * @date 2016.09.20
+ */
+public class GitOperTest {
+
+    @Test
+    public void testFirst(){
+
+    }
+
+}
